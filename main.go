b4791b313517f01f52b149decceed5555fe6ea57
Marco Franssen <marco.franssen@philips.com>
2020-02-12 11:09:16 +0100

    Utilize the actions APIs when running garo

--- main.go
+++ main.go
@@ -14,8 +14,8 @@ func main() {
 		log.Fatal("You need to export the environment variable GH_TOKEN")
 	}
 	args := os.Args[1:]
-	if len(args) != 1 {
-		log.Fatal("You need to provide a Github organization as argument to GARO")
+	if len(args) != 2 {
+		log.Fatalf("You need to provide a Github organization and repo as argument to GARO, e.g.:\n  %s my-org my-repo", os.Args[0])
 	}
 
 	ctx := context.Background()
@@ -25,4 +25,20 @@ func main() {
 	repos, _, err := client.Repositories.ListByOrg(ctx, args[0], nil)
 	dieOnErr("Failed listing repos: %s", err)
 	printJSON("Repositories: %s\n", repos)
+
+	runners, _, err := client.ListActionsRunners(ctx, args[0], args[1])
+	dieOnErr("Failed listing runners: %s", err)
+	printJSON("Runners: %s\n", runners)
+
+	downloads, _, err := client.ListActionsRunnersDownloads(ctx, args[0], args[1])
+	dieOnErr("Failed listing runner downloads: %s", err)
+	printJSON("Runner downloads: %s\n", downloads)
+
+	registrationToken, _, err := client.CreateActionRunnersRegistrationToken(ctx, args[0], args[1])
+	dieOnErr("Failed creating registration token: %s", err)
+	printJSON("Create token: %s\n", registrationToken)
+
+	removeToken, _, err := client.CreateActionRunnersRemoveToken(ctx, args[0], args[1])
+	dieOnErr("Failed creating remove token: %s", err)
+	printJSON("Remove token: %s\n", removeToken)
 }
