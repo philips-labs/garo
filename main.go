84782207c1a733262dfddf80df70fdd8de94f10a
Marco Franssen <marco.franssen@philips.com>
2020-02-13 09:02:38 +0100

    Use named variables for clarity in usage

--- main.go
+++ main.go
@@ -18,27 +18,30 @@ func main() {
 		log.Fatalf("You need to provide a Github organization and repo as argument to GARO, e.g.:\n  %s my-org my-repo", os.Args[0])
 	}
 
+	org := args[0]
+	repo := args[1]
+
 	ctx := context.Background()
 	oauthClient := github.PersonalAccessToken(ctx, token)
 	client := github.NewClient(oauthClient)
 
-	repos, _, err := client.Repositories.ListByOrg(ctx, args[0], nil)
+	repos, _, err := client.Repositories.ListByOrg(ctx, org, nil)
 	dieOnErr("Failed listing repos: %s", err)
 	printJSON("Repositories: %s\n", repos)
 
-	runners, _, err := client.Actions.ListRunners(ctx, args[0], args[1])
+	runners, _, err := client.Actions.ListRunners(ctx, org, repo)
 	dieOnErr("Failed listing runners: %s", err)
 	printJSON("Runners: %s\n", runners)
 
-	downloads, _, err := client.Actions.ListRunnersDownloads(ctx, args[0], args[1])
+	downloads, _, err := client.Actions.ListRunnersDownloads(ctx, org, repo)
 	dieOnErr("Failed listing runner downloads: %s", err)
 	printJSON("Runner downloads: %s\n", downloads)
 
-	registrationToken, _, err := client.Actions.CreateRunnersRegistrationToken(ctx, args[0], args[1])
+	registrationToken, _, err := client.Actions.CreateRunnersRegistrationToken(ctx, org, repo)
 	dieOnErr("Failed creating registration token: %s", err)
 	printJSON("Create token: %s\n", registrationToken)
 
-	removeToken, _, err := client.Actions.CreateRunnersRemoveToken(ctx, args[0], args[1])
+	removeToken, _, err := client.Actions.CreateRunnersRemoveToken(ctx, org, repo)
 	dieOnErr("Failed creating remove token: %s", err)
 	printJSON("Remove token: %s\n", removeToken)
 }
